2e58f7cc98848440b05cce27910562c75a62898f
cezara24 <cezarateodorescu8@gmail.com>
2024-03-28 22:07:58 +0200

    changed metadata

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GlobalStyles from 'styles/GlobalStyles';
 import { css } from "styled-components/macro"; //eslint-disable-line
 
@@ -107,6 +107,14 @@ import ThankYouPage from "ThankYouPage.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
+  // adjusts font based on pixel density
+  useEffect(() => {
+    let devicePixelRatio = window.devicePixelRatio || 1;
+    const baseFontSize = 16;
+    const adjustedFontSize = baseFontSize * 1 + ((devicePixelRatio - 1) / 4);
+    document.documentElement.style.fontSize = `${adjustedFontSize}px`;
+  }, []);
+
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
   // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
 
